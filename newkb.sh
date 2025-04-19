9d31151a2ddac6a6e2c46dab633de210530cecd6
motetpaper <194758925+motetpaper@users.noreply.github.com>
2025-04-18 21:53:44 -0700

    Create newkb.sh

---
+++ newkb.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+##
+## newkb.sh
+## job    : creates a new kb template for Jekyll
+## git    : https://github.com/motetpaper/newkb-sh
+## lic    : MIT
+##
+##
+##
+
+if [[ -z $(command -v kbdate) ]]; then
+  echo "You need 'kbdate' to run 'newkb'"
+  echo "Learn more: https://motetpaper.github.com/KB1234"
+  exit
+fi
+
+### one timestmap for current time
+now=$(date +"%s")
+
+if [[ -z "$1" ]]; then
+
+  dt=$(date --date="@$now" +"%F %H:%M:%S")
+  kbdateid=$(kbdate "$dt")
+else
+
+  ## beware of user input
+
+  dt="$1"
+  kbdateid=$(kbdate "$1")
+fi
+
+outfile="$kbdateid.md"
+
+if [[ $(find . -name $outfile) ]];then
+  echo "$outfile already exists."
+  exit
+fi
+
+echo "Saving $outfile  ..."
+
+echo "..." && cat << MOO > $outfile && echo "done."
+---
+title: Title Here
+date: ${dt}
+updated: ${dt}
+kbdate: ${kbdateid}
+mermaid: false
+mathjax: false
+---
+
+# Summary
+
+This is the summary.
+
+# Introduction
+
+This is the introduction.
+
+# More Information
+
+This is more information.
+
+# References
+
+These are references.
+
+MOO
